a71e621d6ff058f0a43242ac7e231f747643e21d
Marshall Ross <MarshallARoss@gmail.com>
2022-07-04 13:35:00 -0700

    second commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Marshall  on 7/4/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
