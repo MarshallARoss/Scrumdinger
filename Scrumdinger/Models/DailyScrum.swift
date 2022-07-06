54ec38b7fedf5593721e845ebb9c8a0c7a1094b5
Marshall Ross <MarshallARoss@gmail.com>
2022-07-05 17:07:46 -0700

    added edit view

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -11,7 +11,7 @@ struct DailyScrum: Identifiable {
     
     var id: UUID
     var title: String
-    var attendees: [String]
+    var attendees: [Attendee]
     var lengthInMinutes: Int
     var theme: Theme
     
@@ -19,12 +19,32 @@ struct DailyScrum: Identifiable {
     {
         self.id = id
         self.title = title
-        self.attendees = attendees
+        self.attendees = attendees.map { Attendee(name: $0) }
         self.lengthInMinutes = lengthInMinutes
         self.theme = theme
     }
     
 }
+
+extension DailyScrum {
+    
+    struct Attendee: Identifiable {
+        let id = UUID()
+        var name: String
+    }
+    
+    struct Data {
+        var title: String = ""
+        var attendees: [Attendee] = []
+        var lengthInMinutes: Double = 5
+        var theme: Theme = .seafoam
+    }
+    
+    var data: Data {
+        Data(title: title, attendees: attendees, lengthInMinutes: Double(lengthInMinutes), theme: theme)
+    }
+}
+
 
 extension DailyScrum {
     static let sampleData: [DailyScrum] =
